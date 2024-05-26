9bdee205c86b7ed220929d40657d8abb61ef5632
eyalken <10102064+eyalken@users.noreply.github.com>
2024-05-26 17:42:38 +0300

    Update template.py

--- template.py
+++ template.py
@@ -2,7 +2,7 @@
 # create discount system
 # no need to serialze to/from DB - 
 # never change for each product seperatly - create rules
-# first roll 10% discount for all products
+# first rule 10% discount for all products
 # change the structure just once (not in secod phase) 
 # no cart 
 #
