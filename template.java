062546f233109325df1b78de8be9f1d0b8039ba7
eyalken <10102064+eyalken@users.noreply.github.com>
2024-05-28 17:47:23 +0300

    Update template.java

--- template.java
+++ template.java
@@ -1,7 +1,7 @@
 # create discount system
 # no need to serialze to/from DB - 
 # never change for each product seperatly - create rules
-# first role 10% discount for all products
+# first rule 10% discount for all products
 # change the structure just once (not in secod phase) 
 # no cart 
 
